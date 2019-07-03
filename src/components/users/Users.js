02b3d32b1fb5ecb5df466996eb6984b835134af3
Reed Stewart <reedostewart@gmail.com>
2019-07-03 14:08:26 +1200

    useContext hook now introduced, refactored code so App.js file is cleaner than before when just using hooks. No more prop drilling as state is held in a higher context. State, Reducer, and Context files created to hold state from different parts of the application

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
 
-const Users = ({ users, loading }) => {
   if (loading) {
-    return <Spinner />
+    return <Spinner />;
   } else {
     return (
       <div style={userStyle}>
@@ -14,19 +16,14 @@ const Users = ({ users, loading }) => {
           <UserItem key={user.id} user={user} />
         ))}
       </div>
-    )
+    );
   }
-}
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
-}
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
-}
+};
 
-export default Users
+export default Users;
