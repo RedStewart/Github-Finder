3a02772f3ae1afa206c421b7e37373206d8947aa
Reed Stewart <reedostewart@gmail.com>
2019-07-03 14:27:15 +1200

    Fixed error in the alertState. If route doesn't exist, user will be redirected to a not found page. Refactor of App.js code

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
-import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
+import AlertContext from './alertContext';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = props => {
